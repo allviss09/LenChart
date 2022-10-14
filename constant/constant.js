6ec0eae830d3223009520065392ef25f7a0cc25d
Nguyen Di Thai <thaind@msm.com.vn>
2022-10-15 00:16:33 +0700

    Init LenChart

---
+++ constant/constant.js
@@ -0,0 +1,42 @@
+const mydata = [
+    {
+        name: "tm-img-01",
+        img: "",
+        description: "This is Kuromi Chart"
+    },
+    {
+        name: "tm-img-02",
+        img: "",
+        description: "This is Kuromi Chart"
+    },
+    {
+        name: "tm-img-03",
+        img: "",
+        description: "This is Kuromi Chart"
+    },
+    {
+        name: "tm-img-04",
+        img: "",
+        description: "This is Kuromi Chart"
+    },
+    {
+        name: "tm-img-05",
+        img: "",
+        description: "This is Kuromi Chart"
+    },
+    {
+        name: "tm-img-06",
+        img: "",
+        description: "This is Kuromi Chart"
+    },
+    {
+        name: "tm-img-07",
+        img: "",
+        description: "This is Kuromi Chart"
+    },
+    {
+        name: "tm-img-08",
+        img: "",
+        description: "This is Kuromi Chart"
+    }
+];
